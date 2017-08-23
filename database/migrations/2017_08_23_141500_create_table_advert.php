bee32eb6674d53cb8a729d7e1af659818321e59b
kvaxminsk <korol1011@yandex.ru>
2017-08-23 17:49:17 +0300

    add table images and adverts

---
+++ database/migrations/2017_08_23_141500_create_table_advert.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAdvert extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->text('description')->nuallble();
+            $table->integer('status')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
